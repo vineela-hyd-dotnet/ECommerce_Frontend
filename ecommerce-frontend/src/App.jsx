d41349e0ebf0b12aaee89633ec3bac597f377ed5
vineela-hyd-dotnet <vinnujella@gmail.com>
2025-11-25 17:10:10 +0530

    added tailwind css

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-800 border-black rounded m-45">Helloooo</h1>
+    </div>
+  )
+}
+
+export default App
